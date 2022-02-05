fb3f8c2fabe3e8302b7bd4f1e1d5cccf7eb36285
samanthakblack <98419604+samanthakblack@users.noreply.github.com>
2022-02-05 12:03:50 -1000

    Update #description to capitalize

--- src/index.js
+++ src/index.js
@@ -41,7 +41,7 @@ function showWeather(response) {
     response.data.main.temp
   );
   document.querySelector("#description").innerHTML =
-    response.data.weather[0].main;
+    response.data.weather[0].description;
   document.querySelector("#humidity").innerHTML = response.data.main.humidity;
   document.querySelector("#wind").innerHTML = Math.round(
     response.data.wind.speed
